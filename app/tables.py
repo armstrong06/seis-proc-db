d2fca69dadf2f1b33ec11b94c6d40dcb112664f5
aarmstrong06 <aa.armstrong6@gmail.com>
2025-03-23 17:02:58 -0600

    Add class doc strings

--- app/tables.py
+++ app/tables.py
@@ -15,7 +15,15 @@ class FMEnum(enum.Enum):
     DN = "dn"
 
 class ISAMethod(Base):
-    """Abstract class from creating IS-A Method tables"""
+    """Abstract table from creating IS-A Method tables
+    
+    id: Not meaningful identifier for the method, used as the PK
+    name: Name of the method used
+    desc: Optional. Description of method.
+    path: Optional. Path where relevant files for the method are stored. 
+    last_modified: Automatic field that keeps track of when a method was added to
+        or modified in the database in local time. Does not include fractional seconds.
+    """
     __abstract__ = True
     id: Mapped[int] = mapped_column(Integer, autoincrement=True, primary_key=True)
     name: Mapped[str] = mapped_column(String(50))
@@ -32,13 +40,13 @@ class Station(Base):
         id: Not meaningful identifier for the station, used as the PK
         net: Network abbreviation 
         sta: Station code
-        ondate: The datetime in UTC that the station was turned on 
+        ondate: The datetime in UTC that the station was turned on. Does not include fractional seconds.
         lat: station latitude
         lon: station longitude
         elev: station elevation in m
-        offdate: Optional. The datetime in UTC that the station was turned off 
+        offdate: Optional. The datetime in UTC that the station was turned off. Does not include fractional seconds.
         last_modified: Automatic field that keeps track of when a station was added to
-        or modified in the database in local time. 
+        or modified in the database in local time. Does not include fractional seconds.
     """
     __tablename__ = "station"
     id: Mapped[int] = mapped_column(Integer, autoincrement=True, primary_key=True)
@@ -79,7 +87,27 @@ class Channel(Base):
     """Stores a channel's information.
 
     Args:
-        Base (_type_): _description_
+        id: Not meaningful channel identifier that is used as the PK. 
+        sta_id: id of a Station
+        seed_code: SEED Channel name
+        loc: Location code
+        ondate: Datetime the channel was turned on in UTC. Does not include fractional seconds.
+        samp_rate: Instrument sampling rate
+        clock_drift: Instrument clock drift
+        sensit_units: Instrument sensitivity input units
+        sensit_freq: Frequency the instrument sensitivity is measured at
+        sensit_val: Instrument sensitivity value
+        lat: Instrument latitude
+        lon: Instrument longitude
+        elev: Instrument elevation in m
+        depth: Instrument depth in m
+        azimuth: Instrument orientation azimuth
+        dip: Instrument orientation dip
+        offdate: Optional. The datetime the channel was turned off in UTC. Does not include fractional seconds.
+        overall_gain_vel: Optional. The overall gain of the instrument computed in M/S
+        sensor_desc: Optional. The short description of the sensor, usually including the brand
+        last_modified: Automatic field that keeps track of when a row was added to
+                or modified in the database in local time. Does not include microseconds.
     """
     __tablename__ = "channel"
     id: Mapped[int] = mapped_column(Integer, autoincrement=True, primary_key=True)
@@ -134,7 +162,30 @@ class DailyContDataInfo(Base):
     """Keep track of information relating to daily continuous data files used in algorithms.
 
     Args:
-        Base (_type_): _description_
+        id: Not meaningful identifier that is used as the PK
+        sta_id: id of the related Station
+        chan_pref: First two letters of the SEED code for the channels used
+        date: Date the data was recorded
+        samp_rate: Sampling rate of the data (may be different than the Channel samp_rate)
+        dt: Sampling interval of the data (should be 1/samp_rate)
+        org_npts: Number of data samples in the file before any processing is done in ApplyDetectors
+        org_start: Starttime (UTC) of the file before any processing is done in ApplyDetectors.
+            DOES include fractional seconds.
+        org_end: Endtime (UTC) of the file before any processing is done in ApplyDetectors.
+            DOES include fractional seconds.
+        proc_npts: Optional. Number of data samples in the file after processing in ApplyDetectors.
+            This will be the same for the saved posterior probabilities files.
+        proc_start: Optional. Starttime (UTC) of the file after processing in ApplyDetectors.
+            This will be the same for the saved posterior probabilities files.
+            DOES include fractional seconds.
+        proc_end: Optional. Endtime (UTC) of the file after processing in ApplyDetectors.
+            This will be the same for the saved posterior probabilities files.
+            DOES include fractional seconds.
+        prev_appended: Optional. Boolean value that stores whether data from the end of the previous
+            day was appended to the start of this file when processing in ApplyDetectors.
+        error: Optional. Short error string or message indicating why this data was not used in ApplyDetectors.
+        last_modified: Automatic field that keeps track of when a row was added to
+                or modified in the database in local time. Does NOT include fractional seconds.
     """
     __tablename__ = "contdatainfo"
 
@@ -187,24 +238,53 @@ class DailyContDataInfo(Base):
                  f"prev_appended={self.prev_appended!r}, error={self.error!r}, last_modified={self.last_modified!r}")
     
 class RepickerMethod(ISAMethod):
+    """Stores some info about the type/version of phase repicking model or technique used. 
+
+    Args:
+        ISAMethod (_type_)
+
+        phase: Optional. Phase type the model was designed for, if applicable. 
+
+    """
     __tablename__ = "repicker_method"
 
     # One-to-Many relationship with PickCorrection
     corrs: Mapped[List["PickCorrection"]] = relationship(back_populates="method")
 
 class CalibrationMethod(ISAMethod):
+    """Stores some info about the type/version of calibration model or technique used. 
+
+    Args:
+        ISAMethod (_type_)
+
+        phase: Optional. Phase type the model was designed for, if applicable. 
+
+    """
     __tablename__ = "calibration_method"
 
     # One-to-Many relationship with CredibleIntervals
     cis: Mapped[List["CredibleInterval"]] = relationship(back_populates="method")
 
 class FMMethod(ISAMethod):
+    """Stores some info about the type/version of first motion classifier used. 
+
+    Args:
+        ISAMethod (_type_)
+    """
     __tablename__ = "fm_method"
 
     # One-to-Many relationship with FM
     fms: Mapped[List["FirstMotion"]] = relationship(back_populates="method")
 
 class DetectionMethod(ISAMethod):
+    """Stores some info about the type/version of phase Detection algorithm used. 
+
+    Args:
+        ISAMethod (_type_)
+
+        phase: Optional. Phase type the model was designed for, if applicable. 
+
+    """
     __tablename__ = "detection_method"
     phase: Mapped[str] = mapped_column(String(4))
 
@@ -212,6 +292,21 @@ class DetectionMethod(ISAMethod):
     dldets: Mapped[List["DLDetection"]] = relationship(back_populates="method")
 
 class DLDetection(Base):
+    """Store Deep-Learning (DL) phase detections from a certain continuous data file and detection method.
+
+    Args:
+        Base (_type_): _description_
+        id: Not meaningful detection identifier that is used as the PK.
+        data_id: ID of DailyContDataInfo the detection comes from. 
+        method_id: ID of the DetectionMethod used.
+        sample: Sample in the continous data file defined by DailyContDateInfo the detection is assigned to. 
+            A pick time can be derived for a given detection using the sample and DailyContDateInfo.proc_start
+        phase: Presumed phase type of the detection.
+        width: Width of the spike in the posterior probabilities the detection is associated with. 
+        height: Posterior probability value at the detection sample. Value is expected between 1 and 100 (not 0 and 1).
+        last_modified: Automatic field that keeps track of when a row was added to
+                or modified in the database in local time. Does not include microseconds. 
+    """
     __tablename__ = "dldetection"
     id: Mapped[int] = mapped_column(Integer, autoincrement=True, primary_key=True)
     ## PK (not simplified)
@@ -240,6 +335,23 @@ class DLDetection(Base):
                       CheckConstraint("height > 0 AND height <= 100", name="valid_height"))
 
 class Pick(Base):
+    """Describe a pick, which may be derived from a DLDetection.
+
+    Args:
+        Base (_type_): _description_
+        id: Not meaningful pick identifier that is used as the PK.
+        sta_id: Identifier for the Station the pick was made at
+        chan_pref: First two letters of the SEED code for the channels used
+        phase: Presumed phase type of the pick
+        ptime: DateTime of the pick in UTC. DOES include fractional seconds. If a pick has a PickCorrection,
+            it is NOT included in the ptime value. 
+        auth: Short identifier for the author/creator of the pick (i.e., SPDL, UUSS)
+        snr: Single to noise ratio of pick. TODO: Define a clear method for measuring this
+        amp: Amplitude value of pick. TODO: Define a clear method for measuring this
+        detid: Optional. Identifier for the DLDetection the pick is derived from
+        last_modified: Automatic field that keeps track of when a row was added to
+                or modified in the database in local time. Does not include microseconds. 
+    """
     __tablename__ = "pick"
 
     id: Mapped[int] = mapped_column(Integer, autoincrement=True, primary_key=True)
@@ -273,6 +385,24 @@ class Pick(Base):
                       CheckConstraint("amp > 0", name="positive_amp"))
     
 class PickCorrection(Base):
+    """Correction to a Pick to improve the arrival time estimate. Basically assumes some sampling method.
+
+    Args:
+        Base (_type_): 
+        id: Not meaningful pick correction identifier that is used as the PK.
+        pid: Identifer of the Pick the correction is associated with.
+        method_id: Identifier of the RepickerMethod used.
+        median: Median value of all samples
+        mean: Mean value of all samples
+        std: Standard deviation of all samples
+        if_low: Lower inner fence value for all samples
+        if_high: Upper inner fence value for all samples
+        trim_median: Median value of samples within the inner fence
+        trim_mean: Mean value of samples within the inner fence
+        preds: #TODO: Some representation of the predictions, either actually storing them or a path to them... 
+        last_modified: Automatic field that keeps track of when a row was added to
+                or modified in the database in local time. Does not include microseconds. 
+    """
     __tablename__ = "pick_corr"
 
     id: Mapped[int] = mapped_column(Integer, autoincrement=True, primary_key=True)
@@ -306,6 +436,21 @@ class PickCorrection(Base):
                       CheckConstraint("std > 0", name="positive_std"),)
 
 class FirstMotion(Base):
+    """First motion information associated with a P pick
+
+    Args:
+        Base (_type_): _description_
+
+        id: Not meaningful first motion identifier that is used as the PK.
+        pid: Identifer of the Pick the first motion is associated with.
+        method_id: Identifier of the FMMethod used.
+        clsf: First motion classification, must be "uk" (unknown), "up" or "dn" (down) 
+        prob_up: Optional. Probability of the fm being up.
+        prob_dn: Optional. Probability of the fm being down.
+        preds: #TODO: Some representation of the predictions, either actually storing them or a path to them...
+        last_modified: Automatic field that keeps track of when a row was added to
+                or modified in the database in local time. Does not include microseconds.
+    """
     __tablename__ = "fm"
     id: Mapped[int] = mapped_column(Integer, autoincrement=True, primary_key=True)
     ## PK (not simplified)
@@ -328,6 +473,20 @@ class FirstMotion(Base):
                       CheckConstraint("prob_dn >= 0", name="nonneg_prob_dn"))
     
 class CredibleInterval(Base):
+    """Credible Intervals associated with a pick correction.
+
+    Args:
+        Base (_type_): _description_
+        
+        id: Not meaningful first motion identifier that is used as the PK.
+        corr_id: Identifer of the PickCorrection the CI is associated with.
+        method_id: Identifier of the CalibratioNMethod used.
+        percent: Percentage that the CI represents
+        lb: CI lower bound on the pick correction
+        ub: CI upper bound on the pick correction
+        last_modified: Automatic field that keeps track of when a row was added to
+                or modified in the database in local time. Does not include microseconds.
+    """
     __tablename__ = "ci"
     id: Mapped[int] = mapped_column(Integer, autoincrement=True, primary_key=True)
     ## PK (not simplified)
@@ -348,6 +507,24 @@ class CredibleInterval(Base):
                       CheckConstraint("percent > 0 AND percent <= 100", name="valid_percent"))
     
 class Gap(Base):
+    """Information on gaps in the DailyContinuousData for a Channel. Many small gaps may be represented as one 
+    large gap and, if so, avail_sig_sec will be > 0. 
+
+    Args:
+        Base (_type_): _description_
+
+        id: Not meaningful gap identifier that is used as the PK.
+        data_id: ID of DailyContDataInfo the gap comes from.
+        chan_id: ID of the Channel the gap is from. 
+        start: Start time of the gap in UTC. Should include fractional seconds.
+        end: End time of the gap in UTC. Should include fractional seconds.
+        # TODO: Am I going to put entire missing days as a gap?
+        startsamp: Optional. Start sample of the gap in the processed DailyContDataInfo (i.e., the Post Probs)
+        endsamp: Optional. End sample of the gap in the processed DailyContDataInfo (i.e., the Post Probs)
+        avail_sig_sec: If the gap is not continuous, stores the amount of signal (in seconds) that are available.
+        last_modified: Automatic field that keeps track of when a row was added to
+                        or modified in the database in local time. Does not include microseconds.
+    """
     __tablename__ = "gap"
     id: Mapped[int] = mapped_column(Integer, autoincrement=True, primary_key=True)
     ## PK (not simplified)
@@ -373,6 +550,25 @@ class Gap(Base):
                       CheckConstraint("avail_sig_sec >= 0", name="nonneg_avail_sig"))
     
 class Waveform(Base):
+    """Waveform snippet recorded on a Channel, around a Pick, extracted from continuous data described in 
+    DailyContDataInfo.
+
+    Args:
+        Base (_type_): _description_
+        id: Not meaningful waveform identifier that is used as the PK.
+        data_id: ID of DailyContDataInfo describing where the waveform was grabbed from.
+        chan_id: ID of the Channel recording the waveform. 
+        pick_id: ID of the Pick the waveform is centered on. 
+        filt_low: Optional. Lower end of the filter applied. 
+        filt_high: Optional. Upper end of the filter applied.
+        # TODO: Figure out what to store here
+        data: Waveform data in some format of path to data...
+        start: Start time of the waveform in UTC. Should include fractional seconds.
+        end: End time of the waveform in UTC. Should include fractional seconds.
+        proc_notes: Optional. Brief notes about waveform processing.
+        last_modified: Automatic field that keeps track of when a row was added to
+                        or modified in the database in local time. Does not include microseconds.
+    """
     __tablename__ = "waveform"
     id: Mapped[int] = mapped_column(Integer, autoincrement=True, primary_key=True)
     ## PK (not simplified)
