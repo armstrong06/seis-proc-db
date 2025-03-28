88639f53a34f7dc3d59391d1dcac722adc3302b3
aarmstrong06 <aa.armstrong6@gmail.com>
2025-03-28 16:16:34 -0600

    add simple insert functions and tests

--- src/seis_proc_db/services.py
+++ src/seis_proc_db/services.py
@@ -75,8 +75,9 @@ def get_station(session, net, sta, ondate):
     # This should only happen if (oddly) there were Stations with ondates within 1 min
     raise ValueError("More than one Station matching these criteria")
 
+
 def insert_channels(session, channel_dict_list):
-    """Inserts one or more channels into the database using a bulk insert. 
+    """Inserts one or more channels into the database using a bulk insert.
 
     Args:
         session (Session): database session
@@ -89,6 +90,7 @@ def insert_channels(session, channel_dict_list):
     # inserted_channels = session.scalars(stmt, channel_dict_list).all()
 
     session.execute(insert(Channel), channel_dict_list)
+
 
 def insert_channel(session, channel_dict):
     """Insert a single channel into the database.
@@ -106,33 +108,206 @@ def insert_channel(session, channel_dict):
 
     return new_chan
 
-def get_channel(session, sta_id, seed_code):
-    pass
 
-def get_station_channels(session, sta_id):
-    pass
+def get_channel(session, sta_id, seed_code, loc, ondate):
+    """Get a single Channel from the database.
 
-def insert_contdatainfo(session, sid):
-    pass
+    Args:
+        session (Session): database Session
+        sta_id (int): Station id
+        seed_code (str): Channel seed code
+        loc (str): Channel location code
+        ondate (datetime): Channel ondate. Will look for ondates within 1 minutes of this time.
+
+    Returns:
+        Channel: Channel object containing the desired channel information or None.
+    """
+    # stmt = select(Channel).where(
+    #     Channel.sta_id == sta_id,
+    #     Channel.seed_code == seed_code,
+    #     Channel.loc == loc,
+    #     Channel.ondate == ondate
+    # )
+    # result = session.scalars(stmt).all()
+
+    textual_sql = text(
+        "SELECT * FROM channel WHERE sta_id = :sid AND seed_code = :c AND loc = :l AND ABS(TIMESTAMPDIFF(SECOND, ondate, :t)) < 60",
+    )
+    result = session.scalars(
+        select(Channel).from_statement(textual_sql),
+        {"sid": sta_id, "c": seed_code, "l": loc, "t": ondate},
+    ).all()
+
+    if len(result) == 1:
+        return result[0]
+    if len(result) == 0:
+        return None
+
+
+def get_all_station_channels(session, sta_id):
+    """Get a list of Channel objects belonging to one station.
+
+    Args:
+         session (Session): database Session
+        sta_id (int): Station id
+
+    Returns:
+        List: list of Channel objects corresponding to the table rows
+    """
+    stmt = select(Channel).where(
+        Channel.sta_id == sta_id,
+    )
+    result = session.scalars(stmt).all()
+
+    return result
+
+
+def get_common_station_channels(session, sta_id, seed_code_pref):
+    """Get a list of Channel objects belonging to one station with a common sensor type.
+
+    Args:
+        session (Session): database Session
+        sta_id (int): Station id
+        seed_code_pref(str): First two letters of the SEED code for the channel type to gather.
+
+    Returns:
+        List: list of Channel objects corresponding to the table rows
+    """
+
+    if len(seed_code_pref) != 2:
+        return
+
+    seed_code_pref += "."
+
+    stmt = select(Channel).where(
+        Channel.sta_id == sta_id, Channel.seed_code.op("REGEXP")(seed_code_pref)
+    )
+    result = session.scalars(stmt).all()
+
+    # textual_sql = text(
+    #     "SELECT * FROM channel WHERE sta_id = :sid AND seed_code = :c",
+    # )
+    # result = session.scalars(
+    #     select(Channel).from_statement(textual_sql), {"sid": sta_id, "c": seed_code_pref}
+    # ).all()
+
+    return result
+
+
+def insert_contdatainfo(session, contdatainfo_dict):
+    new_contdatainfo = DailyContDataInfo(**contdatainfo_dict)
+    session.add(new_contdatainfo)
+
+    return new_contdatainfo
+
+
+def insert_detection_method(session, name, phase=None, desc=None, path=None):
+    new_det_method = DetectionMethod(name=name, phase=phase, desc=desc, path=path)
+    session.add(new_det_method)
+
+    return new_det_method
+
+
+def insert_dldetection(session, data_id, method_id, sample, phase, width, height):
+    # TODO: Add gap check
+    new_det = DLDetection(
+        data_id=data_id,
+        method_id=method_id,
+        sample=sample,
+        phase=phase,
+        width=width,
+        height=height,
+    )
+    session.add(new_det)
+    return new_det
+
+
+def insert_pick(
+    session, sta_id, chan_pref, phase, ptime, auth, snr=None, amp=None, detid=None
+):
+    new_pick = Pick(
+        sta_id=sta_id,
+        chan_pref=chan_pref,
+        phase=phase,
+        ptime=ptime,
+        auth=auth,
+        snr=snr,
+        amp=amp,
+        detid=detid,
+    )
+    session.add(new_pick)
+
+    return new_pick
+
+
+def insert_gap(
+    session,
+    data_id,
+    chan_id,
+    start,
+    end,
+    startsamp=None,
+    endsamp=None,
+    avail_sig_sec=None,
+):
+    new_gap = Gap(
+        data_id=data_id,
+        chan_id=chan_id,
+        start=start,
+        end=end,
+        startsamp=startsamp,
+        endsamp=endsamp,
+        avail_sig_sec=avail_sig_sec,
+    )
+    session.add(new_gap)
+
+    return new_gap
+
+
+def insert_waveform(
+    session,
+    data_id,
+    chan_id,
+    pick_id,
+    start,
+    end,
+    data,
+    filt_low=None,
+    filt_high=None,
+    proc_notes=None,
+):
+    new_wf = Waveform(
+        data_id=data_id,
+        chan_id=chan_id,
+        pick_id=pick_id,
+        start=start,
+        end=end,
+        data=data,
+        filt_low=filt_low,
+        filt_high=filt_high,
+        proc_notes=proc_notes,
+    )
+    session.add(new_wf)
+
+    return new_wf
+
 
 def get_contdatainfo(session):
     pass
 
-def insert_dldetection(session):
+
+# Potentially useful to implement
+def check_gaps(session):
     pass
 
 
-def insert_pick(session):
+def add_dldetection_pick(session):
     pass
 
 
-def insert_detection_method(session):
+def bulk_insert_dldetections(session):
     pass
 
 
-def insert_gap(session):
-    pass
-
-
-def insert_waveform(session):
+def bulk_insert_picks(session):
     pass
--- tests/test_services.py
+++ tests/test_services.py
@@ -2,41 +2,130 @@ from datetime import datetime
 import pytest
 from copy import deepcopy
 from sqlalchemy import func
+import numpy as np
 
 from seis_proc_db import services, tables
 
 dateformat = "%Y-%m-%dT%H:%M:%S.%f"
 
 
-def test_insert_station(db_session):
-    d = {
-        "ondate": datetime.strptime("1993-10-26T00:00:00.00", dateformat),
-        "net": "WY",
-        "sta": "YNR",
-        "lat": 44.7155,
-        "lon": -110.67917,
-        "elev": 2336,
-    }
+@pytest.fixture
+def stat_ex():
+    return deepcopy(
+        {
+            "ondate": datetime.strptime("1993-10-26T00:00:00.00", dateformat),
+            "net": "WY",
+            "sta": "YNR",
+            "lat": 44.7155,
+            "lon": -110.67917,
+            "elev": 2336,
+        }
+    )
 
-    inserted_stat = services.insert_station(db_session, **d)
-    db_session.commit()
-    assert db_session.get(tables.Station, inserted_stat.id).sta == "YNR"
+
+@pytest.fixture()
+def channel_ex():
+    return deepcopy(
+        {
+            "seed_code": "HHZ",
+            "loc": "01",
+            "ondate": datetime.strptime("1993-10-26T00:00:00.00", dateformat),
+            "samp_rate": 100.0,
+            "clock_drift": 1e-5,
+            "sensor_desc": "Nanometrics something or other",
+            "sensit_units": "M/S",
+            "sensit_val": 9e9,
+            "sensit_freq": 5,
+            "lat": 44.7155,
+            "lon": -110.67917,
+            "elev": 2336,
+            "depth": 100,
+            "azimuth": 90,
+            "dip": -90,
+        }
+    )
+
 
 @pytest.fixture
-def db_session_with_station(db_session):
-    d = {
-        "ondate": datetime.strptime("1993-10-26T00:00:00.00", dateformat),
-        "net": "WY",
-        "sta": "YNR",
-        "lat": 44.7155,
-        "lon": -110.67917,
-        "elev": 2336,
-    }
+def contdatainfo_ex():
+    return deepcopy(
+        {
+            "chan_pref": "HH",
+            "ncomps": 3,
+            "date": datetime(year=2024, month=10, day=1),
+            "samp_rate": 100.0,
+            "dt": 0.01,
+            "org_npts": 86399,
+            "org_start": datetime.strptime("2024-10-01T00:00:00.05", dateformat),
+            "org_end": datetime.strptime("2024-10-01T23:59:59.55", dateformat),
+        }
+    )
 
-    inserted_stat = services.insert_station(db_session, **d)
+
+@pytest.fixture
+def detection_method_ex():
+    return deepcopy(
+        {
+            "name": "P-UNET-v1",
+            "phase": "P",
+            "desc": "For P picks, from Armstrong 2023 BSSA paper",
+            "path": "the/model/files/are/stored/here",
+        }
+    )
+
+
+@pytest.fixture
+def pick_ex():
+    return deepcopy(
+        {
+            "chan_pref": "HH",
+            "phase": "P",
+            "ptime": datetime.strptime("2024-01-02T10:11:12.13", dateformat),
+            "auth": "SPDL",
+            "snr": 40.5,
+            "amp": 10.22,
+        }
+    )
+
+
+@pytest.fixture
+def gap_ex():
+    return deepcopy(
+        {
+            "start": datetime.strptime("2023-01-02T12:13:14.15", dateformat),
+            "end": datetime.strptime("2023-01-02T12:13:14.25", dateformat),
+            "startsamp": 4399415,
+            "endsamp": 4399425,
+        }
+    )
+
+
+@pytest.fixture
+def waveform_ex():
+    return deepcopy(
+        {
+            "filt_low": 1.5,
+            "filt_high": 17.5,
+            "start": datetime.strptime("2024-01-02T10:11:02.13", dateformat),
+            "end": datetime.strptime("2024-01-02T10:11:22.14", dateformat),
+            "proc_notes": "Processed for repicker",
+            "data": np.zeros((2000)).tolist(),
+        }
+    )
+
+
+@pytest.fixture
+def db_session_with_station(db_session, stat_ex):
+    inserted_stat = services.insert_station(db_session, **stat_ex)
     db_session.commit()
 
     return db_session, inserted_stat.id
+
+
+def test_insert_station(db_session_with_station):
+    db_session, sid = db_session_with_station
+    assert db_session.get(tables.Station, sid).sta == "YNR"
+
 
 def test_get_station(db_session_with_station):
     db_session, sid = db_session_with_station
@@ -44,7 +133,9 @@ def test_get_station(db_session_with_station):
     # Just in case it would grab the stored object from the Session
     db_session.expunge_all()
 
-    selected_stat = services.get_station(db_session, "WY", "YNR", datetime.strptime("1993-10-26T00:00:00.00", dateformat))
+    selected_stat = services.get_station(
+        db_session, "WY", "YNR", datetime.strptime("1993-10-26T00:00:00.00", dateformat)
+    )
     assert selected_stat is not None, "station was not found"
     assert (
         selected_stat.lat == 44.7155
@@ -59,28 +150,18 @@ def test_station_no_results(db_session):
     selected_stat = services.get_station(db_session, "WY", "YNR", ondate)
     assert selected_stat is None, "get_station did not return None"
 
-def test_insert_channels(db_session_with_station):
+
+def test_insert_channels(db_session_with_station, channel_ex):
     db_session, sid = db_session_with_station
 
-    common_chan_dict = {
-        "sta_id": sid,
-        "loc": "01",
-        "ondate": datetime.strptime("1993-10-26T00:00:00.00", dateformat),
-        "samp_rate": 100.0,
-        "clock_drift": 1e-5,
-        "sensor_desc": "Nanometrics something or other",
-        "sensit_units": "M/S",
-        "sensit_val": 9e9,
-        "sensit_freq": 5,
-        "lat": 44.7155,
-        "lon": -110.67917,
-        "elev": 2336,
-        "depth": 100,
-        "azimuth": 90,
-        "dip": -90,
-    }
+    common_chan_dict = channel_ex
+    common_chan_dict["sta_id"] = sid
 
-    c1, c2, c3 = deepcopy(common_chan_dict), deepcopy(common_chan_dict), deepcopy(common_chan_dict)
+    c1, c2, c3 = (
+        deepcopy(common_chan_dict),
+        deepcopy(common_chan_dict),
+        deepcopy(common_chan_dict),
+    )
 
     c1["seed_code"] = "HHE"
     c2["seed_code"] = "HHN"
@@ -93,29 +174,223 @@ def test_insert_channels(db_session_with_station):
     cnt = db_session.execute(func.count(tables.Channel.id)).one()
     assert cnt[0] == 3
 
-def test_insert_channel(db_session_with_station):
+
+@pytest.fixture
+def db_session_with_single_channel(db_session_with_station, channel_ex):
     db_session, sid = db_session_with_station
 
-    chan_dict = {
-        "sta_id": sid,
-        "seed_code": "HHZ",
-        "loc": "01",
-        "ondate": datetime.strptime("1993-10-26T00:00:00.00", dateformat),
-        "samp_rate": 100.0,
-        "clock_drift": 1e-5,
-        "sensor_desc": "Nanometrics something or other",
-        "sensit_units": "M/S",
-        "sensit_val": 9e9,
-        "sensit_freq": 5,
-        "lat": 44.7155,
-        "lon": -110.67917,
-        "elev": 2336,
-        "depth": 100,
-        "azimuth": 90,
-        "dip": -90,
-    }
-
+    chan_dict = channel_ex
+    chan_dict["sta_id"] = sid
     inserted_chan = services.insert_channel(db_session, chan_dict)
     db_session.commit()
 
-    assert db_session.get(tables.Channel, inserted_chan.id).seed_code == "HHZ"
+    return db_session, sid, inserted_chan.id
+
+
+def test_insert_channel(db_session_with_single_channel):
+    db_session, sid, cid = db_session_with_single_channel
+    assert db_session.get(tables.Channel, cid).seed_code == "HHZ"
+
+
+def test_get_channel(db_session_with_single_channel):
+    db_session, sid, cid = db_session_with_single_channel
+    db_session.expunge_all()
+
+    selected_chan = services.get_channel(
+        db_session,
+        sid,
+        "HHZ",
+        "01",
+        datetime.strptime("1993-10-26T00:00:00.00", dateformat),
+    )
+
+    assert selected_chan is not None, "Channel not found"
+    assert selected_chan.sta_id == sid, "Incorrect station id"
+    assert (
+        selected_chan.lat == 44.7155
+        and selected_chan.lon == -110.67917
+        and selected_chan.elev == 2336
+    ), "Incorrect station location"
+
+
+def test_get_all_station_channels(db_session_with_station, channel_ex):
+    db_session, sid = db_session_with_station
+
+    common_chan_dict = channel_ex
+    common_chan_dict["sta_id"] = sid
+    c1, c2, c3 = (
+        deepcopy(common_chan_dict),
+        deepcopy(common_chan_dict),
+        deepcopy(common_chan_dict),
+    )
+
+    c1["seed_code"] = "HHE"
+    c2["seed_code"] = "HHN"
+    c3["seed_code"] = "HHZ"
+
+    print(c1["seed_code"], c2["seed_code"], c3["seed_code"])
+
+    services.insert_channels(db_session, [c1, c2, c3])
+    db_session.commit()
+    db_session.expunge_all()
+
+    chan_list = services.get_all_station_channels(db_session, sid)
+
+    assert len(chan_list) == 3, "Incorrect number of Channels"
+
+
+def test_get_common_station_channels(db_session_with_station, channel_ex):
+    db_session, sid = db_session_with_station
+
+    common_chan_dict = channel_ex
+    common_chan_dict["sta_id"] = sid
+    c1, c2, c3, c4 = (
+        deepcopy(common_chan_dict),
+        deepcopy(common_chan_dict),
+        deepcopy(common_chan_dict),
+        deepcopy(common_chan_dict),
+    )
+
+    c1["seed_code"] = "HHE"
+    c2["seed_code"] = "HHN"
+    c3["seed_code"] = "HHZ"
+    c4["seed_code"] = "EHZ"
+
+    services.insert_channels(db_session, [c1, c2, c3, c4])
+    db_session.commit()
+    db_session.expunge_all()
+
+    chan_list = services.get_common_station_channels(db_session, sid, "HH")
+    assert len(chan_list) == 3, "Incorrect number of Channels"
+
+
+@pytest.fixture
+def db_session_with_contdatainfo(db_session_with_station, contdatainfo_ex):
+    db_session, sid = db_session_with_station
+
+    d = contdatainfo_ex
+    d["sta_id"] = sid
+
+    inserted_contdatainfo = services.insert_contdatainfo(db_session, d)
+    db_session.commit()
+
+    return db_session, sid, inserted_contdatainfo.id
+
+
+def test_insert_contdatainfo(db_session_with_contdatainfo):
+    db_session, sid, dataid = db_session_with_contdatainfo
+    assert db_session.get(tables.DailyContDataInfo, dataid).chan_pref == "HH"
+
+
+def test_insert_detection_method(db_session, detection_method_ex):
+    d = detection_method_ex
+    inserted_det_meth = services.insert_detection_method(
+        db_session, name=d["name"], phase=d["phase"], desc=d["desc"], path=d["path"]
+    )
+    db_session.commit()
+    assert inserted_det_meth.name == "P-UNET-v1", "incorrect name"
+    assert inserted_det_meth.phase == "P", "incorrect phase"
+
+
+@pytest.fixture
+def db_session_with_dldetection(db_session_with_contdatainfo, detection_method_ex):
+    db_session, sid, dataid = db_session_with_contdatainfo
+    inserted_method = services.insert_detection_method(
+        db_session, **detection_method_ex
+    )
+    db_session.commit()
+
+    d = {"sample": 1000, "phase": "P", "width": 40, "height": 90}
+    inserted_dldet = services.insert_dldetection(
+        db_session, dataid, inserted_method.id, **d
+    )
+    db_session.commit()
+
+    ids = {
+        "sta": sid,
+        "data": dataid,
+        "method": inserted_method.id,
+        "dldet": inserted_dldet.id,
+    }
+
+    return db_session, ids
+
+
+def test_insert_dldetection(db_session_with_dldetection):
+    db_session, ids = db_session_with_dldetection
+
+    inserted_dldet = db_session.get(tables.DLDetection, ids["dldet"])
+
+    assert inserted_dldet.id > 0, "No id"
+    assert inserted_dldet.phase == "P"
+    assert inserted_dldet.sample == 1000
+
+
+@pytest.fixture
+def db_session_with_dldet_pick(db_session_with_dldetection, pick_ex, channel_ex):
+    db_session, ids = db_session_with_dldetection
+    chan_dict = channel_ex
+    chan_dict["sta_id"] = ids["sta"]
+    chan = services.insert_channel(db_session, chan_dict)
+    db_session.commit()
+    ids["chan"] = chan.id
+
+    inserted_pick = services.insert_pick(
+        db_session, sta_id=ids["sta"], detid=ids["dldet"], **pick_ex
+    )
+    db_session.commit()
+
+    ids["pick"] = inserted_pick.id
+
+    return db_session, ids
+
+
+def test_insert_pick(db_session_with_dldet_pick):
+    db_session, ids = db_session_with_dldet_pick
+    inserted_pick = db_session.get(tables.Pick, ids["pick"])
+    assert inserted_pick.id is not None
+    assert inserted_pick.detid == ids["dldet"]
+    assert inserted_pick.phase == "P"
+
+
+@pytest.fixture
+def db_session_with_gap(db_session_with_contdatainfo, channel_ex, gap_ex):
+    db_session, sid, cid = db_session_with_contdatainfo
+    chan_dict = channel_ex
+    chan_dict["sta_id"] = sid
+    chan = services.insert_channel(db_session, chan_dict)
+    db_session.commit()
+
+    inserted_gap = services.insert_gap(
+        db_session, data_id=cid, chan_id=chan.id, **gap_ex
+    )
+    db_session.commit()
+
+    ids = {"sta": sid, "data": cid, "chan": chan.id, "gap": inserted_gap.id}
+
+    return db_session, ids
+
+
+def test_insert_gap(db_session_with_gap):
+    db_session, ids = db_session_with_gap
+    inserted_gap = db_session.get(tables.Gap, ids["gap"])
+    assert inserted_gap.id is not None
+    assert inserted_gap.end > inserted_gap.start, "Invalid times"
+
+
+def test_insert_waveform(db_session_with_dldet_pick, waveform_ex):
+    db_session, ids = db_session_with_dldet_pick
+
+    new_wf = services.insert_waveform(
+        db_session,
+        data_id=ids["data"],
+        chan_id=ids["chan"],
+        pick_id=ids["pick"],
+        **waveform_ex,
+    )
+
+    db_session.commit()
+
+    assert new_wf.id is not None, "ID is not set"
+    assert len(new_wf.data) == 2000, "data length is invalid"
+    assert new_wf.filt_low == 1.5, "filt_low is invalid"
