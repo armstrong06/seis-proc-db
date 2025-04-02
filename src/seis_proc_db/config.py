0b0102f85953da26b8635ff5c22d04ccaa24e45d
aarmstrong06 <aa.armstrong6@gmail.com>
2025-04-02 11:54:17 -0600

    separate vars for db info

--- src/seis_proc_db/config.py
+++ src/seis_proc_db/config.py
@@ -3,7 +3,9 @@ import os
 # Get user and password environmental variables so they are not hardcoded
 USER = os.getenv("USER")
 DB_PASSWORD = os.getenv("MYSQL_PASSWORD")
-DB_URL = f"mysql+mysqldb://{USER}:{DB_PASSWORD}@mysql.chpc.utah.edu/SeisProcML"
+DB_TYPE = "mysql"
+DBAPI = "mysqldb"
+DB_URL = f"{DB_TYPE}+{DBAPI}://{USER}:{DB_PASSWORD}@mysql.chpc.utah.edu/SeisProcML"
 # DB_URL = f"mariadb+mariadbconnector://{USER}:{DB_PASSWORD}@mysql.chpc.utah.edu/SeisProcML"
 
 MYSQL_ENGINE = "InnoDB"
