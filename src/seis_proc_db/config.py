39bda6d87b77f927d22b526221b9956972b431ef
aarmstrong06 <aa.armstrong6@gmail.com>
2025-04-16 20:37:47 -0600

    get database host string from env var instead of hardcoding

--- src/seis_proc_db/config.py
+++ src/seis_proc_db/config.py
@@ -3,9 +3,10 @@ import os
 # Get user and password environmental variables so they are not hardcoded
 USER = os.getenv("USER")
 DB_PASSWORD = os.getenv("MYSQL_PASSWORD")
+DB_HOST = os.getenv("MYSQL_HOST")
 DB_TYPE = "mysql"
 DBAPI = "mysqldb"
-DB_URL = f"{DB_TYPE}+{DBAPI}://{USER}:{DB_PASSWORD}@mysql.chpc.utah.edu/SeisProcML"
+DB_URL = f"{DB_TYPE}+{DBAPI}://{USER}:{DB_PASSWORD}@{DB_HOST}/SeisProcML"
 # DB_URL = f"mariadb+mariadbconnector://{USER}:{DB_PASSWORD}@mysql.chpc.utah.edu/SeisProcML"
 
 MYSQL_ENGINE = "InnoDB"
