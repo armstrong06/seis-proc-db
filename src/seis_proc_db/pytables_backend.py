ec619ae3b6ebd649a41108d8001742178bf7d7a4
aarmstrong06 <aa.armstrong6@gmail.com>
2025-04-18 16:39:10 -0600

    clean up a bit

--- src/seis_proc_db/pytables_backend.py
+++ src/seis_proc_db/pytables_backend.py
@@ -12,13 +12,23 @@ class BasePyTable(ABC):
     TABLE_DESCRIPTION = None
     TABLE_DTYPE = None
 
+    @property
+    def table(self):
+        return self._table
+
+    @property
+    def file_path(self):
+        return self._file_path
+
     def __init__(self, expected_array_length):
 
+        self._is_open = False
         self.expected_array_length = expected_array_length
         self._file_path = self._make_filepath()
         self._flush_counter = 0
         self._flush_threshold = 50
-        self._h5_file, self._table = self._open_file(
+        self._h5_file, self._table = None, None
+        self._open_file(
             self.TABLE_NAME,
             self.TABLE_TITLE,
             self.TABLE_DESCRIPTION,
@@ -36,29 +46,45 @@ class BasePyTable(ABC):
     def _open_file(
         self, table_name, table_title, table_description, table_data_col_type
     ):
+        dir_exists = os.path.exists(os.path.dirname(self._file_path))
+        if not dir_exists:
+            os.makedirs(os.path.dirname(self._file_path))
+
         file_exists = os.path.isfile(self._file_path)
 
-        h5file = open_file(
-            self._file_path,
-            mode="a",
-            title=(self._make_h5_file_title()),
-        )
-
-        if not file_exists:
-            table = h5file.create_table(
-                "/",
-                table_name,
-                self._generate_table_description(
-                    table_data_col_type, table_description
-                ),
-                table_title,
+        h5file = None
+        try:
+            h5file = open_file(
+                self._file_path,
+                mode="a",
+                title=(self._make_h5_file_title()),
             )
 
-            table.cols.id.create_index()
+            if not file_exists:
+                table = h5file.create_table(
+                    "/",
+                    table_name,
+                    self._generate_table_description(
+                        table_data_col_type, table_description
+                    ),
+                    table_title,
+                )
 
-            self._set_table_metadata()
+                table.cols.id.create_index()
 
-        return h5file, table
+                self._set_table_metadata(table)
+            else:
+                table = h5file.get_node(f"/{self.TABLE_NAME}")
+
+            self._h5_file = h5file
+            self._table = table
+            self._is_open = True
+
+        except Exception as e:
+            if h5file is not None:
+                h5file.close()
+
+            raise
 
     def _generate_table_description(self, data_col_type, table_description):
         class_attrs = {
@@ -71,7 +97,11 @@ class BasePyTable(ABC):
     def __enter__(self):
         return self
 
-    def __exit__(self):
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.flush()
+        self.close()
+
+    def __del__(self):
         self.flush()
         self.close()
 
@@ -81,8 +111,8 @@ class BasePyTable(ABC):
             self._table.flush()
             self._flush_counter = 0
 
-    def _set_table_metadata(self):
-        attrs = self._table.attrs
+    def _set_table_metadata(self, table):
+        attrs = table
 
         # Get instance attributes that came from __init__ (excluding private/internal ones)
         init_params = {k: v for k, v in self.__dict__.items() if not k.startswith("_")}
@@ -99,11 +129,14 @@ class BasePyTable(ABC):
                 setattr(attrs, key, value)
 
     def close(self):
-        self._h5_file.close()
+        if self._h5_file is not None and self._is_open:
+            self._h5_file.close()
+            self._is_open = False
 
     def flush(self):
-        self._table.flush()
-        self._flush_counter = 0
+        if self._table is not None and self._is_open and self._flush_counter > 0:
+            self._table.flush()
+            self._flush_counter = 0
 
     def append(self, db_id, data_array):
         row = self._table.row
@@ -125,7 +158,7 @@ class BasePyTable(ABC):
         self._maybe_flush()
 
 
-class WaveformPyTable(BasePyTable):
+class WaveformStorage(BasePyTable):
     TABLE_NAME = "waveform"
     TABLE_TITLE = "Waveform data"
     TABLE_DESCRIPTION = "Waveform"
@@ -154,7 +187,7 @@ class WaveformPyTable(BasePyTable):
         super().__init__(expected_array_length)
 
     def _make_filepath(self):
-        file_name = f"{self.sta}_{self.seed_code}_{self.phase}_{self.ncomps}C_{self.filt_low!r}hz_{self.filt_high!r}hz_{self.expected_array_length}samps.h5"
+        file_name = f"{self.sta}_{self.seed_code}_{self.phase}_{self.ncomps}C_{self.filt_low!r}Hz_{self.filt_high!r}Hz_{self.expected_array_length}samps.h5"
         return os.path.join(HDF_BASE_PATH, HDF_WAVEFORM_DIR, file_name)
 
     def _make_h5_file_title(self):
@@ -164,7 +197,7 @@ class WaveformPyTable(BasePyTable):
         )
 
 
-class DLDetectorOutputPyTable(BasePyTable):
+class DLDetectorOutputStorage(BasePyTable):
 
     TABLE_NAME = "dldetector_output"
     TABLE_TITLE = "DL detector output"
