b86bf651b116c10779e26b84800c2dbf32c04d53
aarmstrong06 <aa.armstrong6@gmail.com>
2025-05-20 15:00:17 -0600

    remove pytables when dropping all db tables

--- scripts/drop_tables.py
+++ scripts/drop_tables.py
@@ -1,5 +1,12 @@
 # Have to import tables or Base doesn't register them
 from seis_proc_db import database, tables
+import os
+from seis_proc_db.config import (
+    HDF_BASE_PATH,
+    HDF_WAVEFORM_DIR,
+    HDF_UNET_SOFTMAX_DIR,
+    HDF_PICKCORR_DIR,
+)
 
 """Drop all tables defined in app.tables
 """
@@ -10,3 +17,9 @@ if __name__ == "__main__":
         print(name)
 
     metadata.drop_all(database.engine)
+
+    for data_dir in [HDF_WAVEFORM_DIR, HDF_UNET_SOFTMAX_DIR, HDF_PICKCORR_DIR]:
+        path = os.path.join(HDF_BASE_PATH, data_dir)
+        if os.path.exists(path):
+            os.rmdir(path)
+            print(f"Directory '{path}' and its contents removed successfully.")
