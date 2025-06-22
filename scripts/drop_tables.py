1f632b77defb9ace3a283d30ac35f780c304fd99
aarmstrong06 <aa.armstrong6@gmail.com>
2025-06-22 16:20:09 -0600

    comment out so I can't accidently drop all tables

--- scripts/drop_tables.py
+++ scripts/drop_tables.py
@@ -1,25 +1,25 @@
-# Have to import tables or Base doesn't register them
-from seis_proc_db import database, tables
-import os
-from seis_proc_db.config import (
-    HDF_BASE_PATH,
-    HDF_WAVEFORM_DIR,
-    HDF_UNET_SOFTMAX_DIR,
-    HDF_PICKCORR_DIR,
-)
+# # Have to import tables or Base doesn't register them
+# from seis_proc_db import database, tables
+# import os
+# from seis_proc_db.config import (
+#     HDF_BASE_PATH,
+#     HDF_WAVEFORM_DIR,
+#     HDF_UNET_SOFTMAX_DIR,
+#     HDF_PICKCORR_DIR,
+# )
 
-"""Drop all tables defined in app.tables
-"""
+# """Drop all tables defined in app.tables
+# """
 
-if __name__ == "__main__":
-    metadata = database.Base.metadata
-    for name, table in metadata.tables.items():
-        print(name)
+# if __name__ == "__main__":
+#     metadata = database.Base.metadata
+#     for name, table in metadata.tables.items():
+#         print(name)
 
-    metadata.drop_all(database.engine)
+#     metadata.drop_all(database.engine)
 
-    for data_dir in [HDF_WAVEFORM_DIR, HDF_UNET_SOFTMAX_DIR, HDF_PICKCORR_DIR]:
-        path = os.path.join(HDF_BASE_PATH, data_dir)
-        if os.path.exists(path):
-            os.rmdir(path)
-            print(f"Directory '{path}' and its contents removed successfully.")
+#     for data_dir in [HDF_WAVEFORM_DIR, HDF_UNET_SOFTMAX_DIR, HDF_PICKCORR_DIR]:
+#         path = os.path.join(HDF_BASE_PATH, data_dir)
+#         if os.path.exists(path):
+#             os.rmdir(path)
+#             print(f"Directory '{path}' and its contents removed successfully.")
